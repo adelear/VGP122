b6beb3667c017c0b90e5b4d5690c5d7c1a5c59ae
Rae Leda <131167141+adelear@users.noreply.github.com>
2023-04-18 13:29:39 -0400

    Add files via upload

---
+++ Lab2/Q4/Main.cpp
@@ -0,0 +1,26 @@
+/* Factorials are used frequently in probability problems. The factorial of a
+positive integer n (written n! and pronounced “n factorial”) is equal to the product
+of the positive integers from 1 to n. Write an application that calculates the
+factorials of 1 through 20. Use type long. Display the results in tabular format. */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int totalNum, factorial=1,counter=1;
+
+	cout << "Enter a number: "; 
+	cin >> totalNum;
+
+	while (counter <= totalNum) {
+		//Starting from the lowest number, multiply factorial with counter until counter reaches the initialized number
+		//Counter increases by 1 each time (1*1, then 1*2, 2*3, 6*4, 24*5)
+		factorial *= counter; 
+		counter++;
+		cout << factorial << endl;
+	}
+
+	cout << "\nThe factorial of " << totalNum << " is " << factorial << endl;
+
+	return 0;
+}
